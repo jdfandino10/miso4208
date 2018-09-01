4d0b0751d1b9e2a714dc60330148cd74ba66902e
Juan Camilo Bages <jcbages95@gmail.com>
2018-08-31 18:52:39 -0700

    Add worker

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var bb = require('express-busboy');
+var cypress = require('cypress');
 var app = express();
+
+var worker = require('./worker');
 
 bb.extend(app, {
     upload: true,
-    path: '/uploadedFiles',
+    path: './uploadedFiles',
     allowedPath: /./
 });
-
 
 app.use(express.static(__dirname + '/view'));
 //Store all HTML files in view folder.
@@ -15,18 +17,20 @@ app.use(express.static(__dirname + '/script'));
 //Store all JS and CSS in Scripts folder.
 
 app.get('/', function(req, res){
-   res.sendFile('index.html');
+    res.sendFile('index.html');
 });
 
 app.get('/main.js', function(req, res){
-   res.sendFile('main.js');
+    res.sendFile('main.js');
 });
 
 app.post('/testupload', function(req, res) {
-  console.log(req.files);
-  console.log(req.body);
-  console.log('hello');
-  res.send('test is "running" (yeah sure)');
+    console.log(req.files);
+    console.log(req.body);
+    console.log('hello');
+
+
 });
 
+worker.init();
 app.listen(3000);
